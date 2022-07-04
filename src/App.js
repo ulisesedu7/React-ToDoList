b831e6ea2c0a55d3984548a55f485eadf544b344
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-07-04 13:27:08 -0600

    Create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React-App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
